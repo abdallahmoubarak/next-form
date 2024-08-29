9965623ce822a3c3967a7d8f13abf4dd095c3ee8
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2024-08-29 15:09:40 +0300

    adding middleware and new auth method

--- auth.ts
+++ auth.ts
@@ -1,34 +1,47 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialProvider from "next-auth/providers/credentials";
 import { Provider } from "next-auth/providers";
+import Credentials from "next-auth/providers/credentials";
+import User from "@/models/user";
+import { signInSchema } from "./utils/zod";
+import bcrypt from "bcryptjs";
+import { connectDB } from "./utils/mongodb";
+import GoogleProvider from "next-auth/providers/google";
 
 const providers: Provider[] = [
-  CredentialProvider({
-    name: "Credentials",
-    credentials: {
-      email: {},
-      password: {},
-    },
-    async authorize(credentials: any) {
+  GoogleProvider,
+  Credentials({
+    authorize: async (credentials) => {
+      connectDB();
       console.log(credentials);
-      return {
-        name: credentials?.name,
-        email: credentials?.email,
-        role: credentials?.role,
-        id: credentials?.id,
-        image: credentials?.image,
-      };
+      let user = null;
+      try {
+        const { email, password } = await signInSchema.parseAsync(credentials);
+        user = await User.findOne({ email });
+        console.log(user);
+        if (!user) throw new Error("Wrong Email");
+
+        const passwordsMatch = await bcrypt.compare(password, user?.password);
+        if (passwordsMatch) return user;
+      } catch (err: any) {
+        throw new Error(err.message);
+      }
+      return user;
     },
   }),
-  GoogleProvider,
 ];
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  // adapter: MongoDBAdapter(clientPromise),
   session: { strategy: "jwt" },
-  pages: {
-    signIn: "/login",
-  },
+  secret: process.env.AUTH_SECRET,
+  pages: { signIn: "/login" },
   providers,
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth;
+    },
+  },
 });
--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,27 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image).*)"],
+};
+
+const protectedRoutes = ["/"];
+const authRoutes = ["/login", "/signup"];
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isAuthRoute = authRoutes.includes(path);
+
+  const session = await auth();
+
+  if (isProtectedRoute && !session?.user?.email) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+
+  if (isAuthRoute && session?.user?.email) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
