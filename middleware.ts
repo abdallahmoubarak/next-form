79cd248aaf64e431b39452481ff4664fc94f2b28
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2024-09-08 20:47:26 +0300

    checking the middleware function

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,27 @@
-import NextAuth from "next-auth";
-import authConfig from "./auth.config";
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./auth";
 
-export const { auth: middleware } = NextAuth(authConfig);
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image).*)"],
+};
+
+const protectedRoutes = ["/"];
+const authRoutes = ["/login", "/signup"];
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  ("use server");
+  const session = await auth();
+
+  if (isProtectedRoute && !session) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+
+  const isAuthRoute = authRoutes.includes(path);
+  if (isAuthRoute && session) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
