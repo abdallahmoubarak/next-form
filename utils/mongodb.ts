9cc682170a5c194d016a6e8d21aaf2d4c5ac4b05
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2024-09-08 13:18:11 +0300

    checking mogodb fixes

--- utils/mongodb.ts
+++ utils/mongodb.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const { MONGO_URI } = process.env;
 export const connectDB = async () => {
   try {
-    if (typeof window !== "undefined") {
+    if (typeof window === "undefined") {
       // Only run this code in the browser
       const { connection } = await mongoose.connect(MONGO_URI as string);
       if (connection.readyState === 1) {
