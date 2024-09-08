aba0abeb1a22286ebabe32dd58cb5d6b0a7cb043
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2024-09-08 13:23:58 +0300

    getting chatgpt fixes

--- utils/mongodb.ts
+++ utils/mongodb.ts
@@ -1,16 +1,20 @@
 import mongoose from "mongoose";
+
 const { MONGO_URI } = process.env;
+
 export const connectDB = async () => {
+  if (!MONGO_URI) {
+    throw new Error("Please add your Mongo URI to the environment variables");
+  }
+
   try {
-    if (typeof window === "undefined") {
-      // Only run this code in the browser
-      const { connection } = await mongoose.connect(MONGO_URI as string);
-      if (connection.readyState === 1) {
-        return Promise.resolve(true);
-      }
+    const { connection } = await mongoose.connect(MONGO_URI);
+    if (connection.readyState === 1) {
+      console.log("MongoDB connected");
+      return true;
     }
   } catch (error) {
-    console.error(error);
-    return Promise.reject(error);
+    console.error("MongoDB connection error:", error);
+    return false;
   }
 };
